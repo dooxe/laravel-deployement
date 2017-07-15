273b422304d9400fadd2acb1080f74dfdba32241
dooxe-creative <maxime.daisy@gmail.com>
2017-07-16 00:24:52 +0200

    Create user-deployement.sh

---
+++ resources/scripts/user-deployement.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#---------------------------------------------------------------------------
+# The goal of this file is to setup your environement before deploying
+# I personnaly use it to create the databases etc 
+#
+# @author dooxe
+#---------------------------------------------------------------------------
